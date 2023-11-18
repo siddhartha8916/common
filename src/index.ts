24a9f15ee3965621aecc470f8a24b7ae4e6cbc5d
siddhartha8916 <siddhartha8916@gmail.com>
2023-11-18 10:25:28 +0530

    Updated Code

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 20,
+  green: 30,
+  blue: 33,
+};
+
+console.log('color', color)
+
+export default color
